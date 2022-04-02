6d9412601ed02e10e0e352b7071614c506fc7d99
etabb98 <emmanueltabb52@gmail.com>
2022-04-02 10:04:45 -0500

    menu upload

---
+++ Game Jam/Assets/pausemenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pausemenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pausemenuUI;
+    
+
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void ControlBack()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
+    public void Resume()
+    {
+        pausemenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+
+    }
+
+     void Pause()
+    {
+        pausemenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true; 
+    }
+
+    public void LoadCONTROLS()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("CONTROLS");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
---
+++ Game Jam/Assets/startmenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class startmenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ScenceSwitcher()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void ControlBack()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
+    public void VolumeBack()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+    }
+
+    public void Controlback2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+    }
+
+
+    public void QuitGame()
+    {
+        Debug.Log(" QUIT!");
+        Application.Quit();
+    }
+}
